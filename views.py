fded422b09af6f465c5028ab80e19237595c180e
Jinal <167493286+05Jinal@users.noreply.github.com>
2024-09-27 11:40:44 +1200

    Add files via upload

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
-
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from vege.models import *
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
@@ -73,3 +75,66 @@ def update_receipe(request, id):
 
     return render(request, 'receipes/update.html', context)
 
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Check if the user exists
+        if not User.objects.filter(username=username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            messages.error(request, 'Invalid password or username.')
+            return redirect('/login/') 
+        else:
+            login(request, user)  # Log the user in
+            return redirect('/receipes/')  # Redirect to recipes page
+
+    return render(request, 'receipes/login.html')  # Render the login page for GET requests
+
+
+def logout_page(request):
+     
+     logout(request)
+     return redirect('/login')   # Redirect to recipes page
+
+
+def register_page(request):
+    
+
+    if request.method=="POST" :
+        first_name= request.POST.get('first_name')
+        last_name= request.POST.get('last_name')
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+
+        user= User.objects.filter(username = username)
+
+        if user.exists():
+            messages.info(request,'Username already taken')
+            return redirect ('/register/')
+
+
+        user= User.objects.create(
+            first_name= first_name,
+            last_name= last_name,
+            username= username
+        )
+        
+        user.set_password(password)
+        user.save()
+
+        messages.success(request, 'Account Created Successfully')  # Correct use of messages
+        return redirect('/register/')
+
+            
+        
+
+    return render(request,'receipes/register.html')
