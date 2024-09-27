2de1d211054e0de7381bbeec4034388d81abb503
Jinal <167493286+05Jinal@users.noreply.github.com>
2024-09-27 12:20:50 +1200

    Update views.py

--- views.py
+++ views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from vege.models import *
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required(login_url="/login/")
 def receipes(request):
     if request.method=="POST":
         data= request.POST
